d9d0d344da74b8e056d7be0f5cf460305c44942a
showaibbinnasir <showaibnishad@gmail.com>
2022-11-25 06:27:05 -0600

    modal added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Product = ({ product }) => {
+const Product = ({ product, setBookedProduct }) => {
     return (
         <div className='text-white'>
             <div className="card card-side bg-base-100 shadow-2xl">
@@ -11,11 +11,22 @@ const Product = ({ product }) => {
                         product.verification === true ? <div className='flex'>
                             <p>Seller name: {product.sellerName}</p>
                             <p><small>Verified</small></p>
-                        </div>:<p>Seller name : {product.sellerName}</p>
+                        </div> : <p>Seller name : {product.sellerName}</p>
                     }
-                    
+                    <p>Original price : {product.original_price} Taka</p>
+                    <p>Resale price : {product.seller_price} Taka</p>
+
                     <div className="card-actions justify-end">
-                        <button className="btn btn-warning">BOOK NOW</button>
+                        
+                       
+                        {
+                            product.verification === true ?
+                             <label onClick={()=> setBookedProduct(product)} className="btn btn-warning" htmlFor="my-modal-3">Book Now</label> :
+                             <label disabled title='not verified' className="btn btn-warning" htmlFor="my-modal-3">Book Now</label>
+                        }
+
+                        
+                        
                     </div>
                 </div>
             </div>
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import ModalOpen from '../Product/ModalOpen';
 import Product from '../Product/Product';
 
 const Products = () => {
     const products = useLoaderData();
+    const [bookedProduct, setBookedProduct] = useState(null);
     console.log(products);
     return (
         <div>
-            <h1 className='text-4xl text-warning font-semibold my-6'>Products</h1>
+            <h1 className='text-4xl text-warning font-semibold my-6'>Products of {products[0].brandName}</h1>
             <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 mx-12'>
                 {
-                    products.map(product => <Product key={product.product_id} product={product}></Product>)
+                    products.map(product => <Product key={product.product_id} product={product} setBookedProduct={setBookedProduct}></Product>)
+                }
+                {
+                    bookedProduct && <ModalOpen bookedProduct={bookedProduct}></ModalOpen>
                 }
             </div>
         </div>
